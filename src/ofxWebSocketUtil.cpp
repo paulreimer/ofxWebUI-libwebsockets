9dce3d8a98864a38994fc1ecb176875e61083c50
Paul Reimer <paulreimer@gmail.com>
2012-03-11 16:05:48 -0700

    Switch to Poco base64 encoding/decoding

--- src/ofxWebSocketUtil.cpp
+++ src/ofxWebSocketUtil.cpp
@@ -8,8 +8,8 @@
  * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  */
 
-#include "encode.h"
-#include "decode.h"
+#include "Poco/Base64Encoder.h"
+#include "Poco/Base64Decoder.h"
 
 #include <sstream>
 #include <string.h>
@@ -19,17 +19,13 @@ unsigned int b64_decode_string(const char* message,
                                char* decoded,
                                const unsigned int len)
 {
-  std::stringstream encoded_stream;
-  encoded_stream << message;
-  
-  std::stringstream decoded_stream;
-  base64::decoder decoder;
-  
-  decoder.decode(encoded_stream, decoded_stream);
-  
-  const std::string& _decoded = decoded_stream.str();
+  std::istringstream preencoded_stream(std::string(message, len));
+  Poco::Base64Decoder decoder(preencoded_stream);
+  std::string _decoded;
+  decoder >> _decoded;
+
   memcpy(decoded, _decoded.c_str(), _decoded.size());
-  
+
   return _decoded.size();
 }
 
@@ -39,17 +35,14 @@ unsigned int b64_encode_string(const char* message,
                                char* encoded,
                                const unsigned int max_encoded_len)
 {
-  std::stringstream binary_stream;
-  binary_stream << std::string(message, len);
-  
-  std::stringstream encoded_stream;
-  base64::encoder encoder;
-  
-  encoder.encode(binary_stream, encoded_stream);
-  
+  std::ostringstream encoded_stream;
+  Poco::Base64Encoder encoder(encoded_stream);
+  encoder << std::string(message, len);
+  encoder.close();
+
   const std::string& _encoded = encoded_stream.str();
   if (_encoded.size() <= max_encoded_len)
     memcpy(encoded, _encoded.c_str(), _encoded.size());
-  
+
   return _encoded.size();
 }
